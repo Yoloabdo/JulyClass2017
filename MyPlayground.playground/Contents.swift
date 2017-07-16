3e3b0acda21c7746f2f09ed2ae8a81b18fb63761
yoloabdo <dhooom.da@gmail.com>
2017-07-16 19:19:29 +0200

    2 classes

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+let name: String = "Ahmed"
+let phone: Int = 012341235418923
+let hex : Double = 324123412341233412
+let point: Float? = nil
+
+
+func addTwoNumbers(x: Int, y: Int) -> Int {
+    return x + y
+}
+
+let results = addTwoNumbers(x: 5, y: 9)
+
